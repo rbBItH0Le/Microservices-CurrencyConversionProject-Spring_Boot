3a3f1195cc51f6d951f759124a771cec24448008
rbBItH0Le <rohan.kishan@yahoo.in>
2022-09-28 18:57:17 +0530

    lastfinal

--- currency-exchange-service/src/main/java/com/we/startproject/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/we/startproject/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.we.startproject;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,8 @@ public class CurrencyExchangeController {
 	@Autowired
 	private CurrencyExchangeRepository currepository;
 	
+	private Logger logger=LoggerFactory.getLogger(CurrencyExchangeController.class);
+	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from,@PathVariable String to)
 	{
@@ -28,6 +32,7 @@ public class CurrencyExchangeController {
 		}
 		String port=environment.getProperty("local.server.port");
 		currencyexchange.setEnvironment(port);
+		logger.info("Retrieve method called with values {} to {}",from,to);
 		return currencyexchange;
 	}
 
